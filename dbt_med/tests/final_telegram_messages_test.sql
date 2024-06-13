248df98fef8217d043d19e4050c118669bf4f3a6
Daniel-Andarge <andargedaniel90@gmail.com>
2024-06-13 23:53:18 +0300

    Add DBT Test

---
+++ dbt_med/tests/final_telegram_messages_test.sql
@@ -0,0 +1,56 @@
+-- tests/final_telegram_messages_test.sql
+
+{{ config(materialized='test') }}
+
+WITH source_data AS (
+  SELECT
+    id,
+    channel,
+    message_id,
+    content,
+    timestamp,
+    views,
+    message_link,
+    message_length
+  FROM {{ ref('final_telegram_messages') }}
+),
+
+validation_checks AS (
+  SELECT
+    id,
+    channel,
+    message_id,
+    content,
+    timestamp,
+    views,
+    message_link,
+    message_length,
+    REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(content, '!', ''), '@', ''), '#', ''), '$', ''), '%', ''), '^', '') AS cleaned_content,
+    LOWER(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(content, '!', ''), '@', ''), '#', ''), '$', ''), '%', ''), '^', '')) AS lowercased_content
+  FROM source_data
+)
+
+SELECT
+  *
+FROM validation_checks
+WHERE
+  -- Check for emojis in content column
+  cleaned_content <> content
+  -- Check for lowercase content
+  OR lowercased_content <> content
+  -- Check for unique row IDs
+  OR id IN (
+    SELECT id
+    FROM validation_checks
+    GROUP BY id
+    HAVING COUNT(*) > 1
+  )
+  -- Check for NULL values
+  OR id IS NULL
+  OR channel IS NULL
+  OR message_id IS NULL
+  OR content IS NULL
+  OR timestamp IS NULL
+  OR views IS NULL
+  OR message_link IS NULL
+  OR message_length IS NULL
